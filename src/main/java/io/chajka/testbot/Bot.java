3012778353d34ed029d72898220389f609d60921
Chajka42 <chajka42y73@gmail.com>
2022-10-21 13:26:40 +0300

    Initial commit

---
+++ src/main/java/io/chajka/testbot/Bot.java
@@ -0,0 +1,67 @@
+package io.chajka.testbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.CopyMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@SpringBootApplication
+public class Bot extends TelegramLongPollingBot {
+
+    public static void main(String[] args) throws TelegramApiException {
+        SpringApplication.run(Bot.class, args);
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        Bot bot = new Bot();                  //We moved this line out of the register method, to access it later
+        botsApi.registerBot(bot);
+        bot.sendText(703699939L, "Hello World!");  //The L just turns the Integer into a Long
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "Speed_Dating_LGBTQ_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5789681007:AAGkY1BCnl-pMKt5ExaZMFr5EL8DOZsCeTY";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+        var ui = update.getMessage().getMessageId();
+
+        copyMessage(id, ui);
+    }
+
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+
+    public void copyMessage(Long who, Integer msgId){
+        CopyMessage cm = CopyMessage.builder()
+                .fromChatId(who.toString())  //We copy from the user
+                .chatId(who.toString())      //And send it back to him
+                .messageId(msgId)            //Specifying what message
+                .build();
+        try {
+            execute(cm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/io/chajka/testbot/TestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.chajka.testbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
